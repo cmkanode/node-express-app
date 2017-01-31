25febf5aaa025cc519f1a540ca3bdbf21cc67979
cmkanode <cm.kanode@gmail.com>
2017-01-30 21:19:27 -0500

    Added Package

--- controllers/v1/planets-controller.js
+++ controllers/v1/planets-controller.js
@@ -10,7 +10,7 @@
             // newDoc is the newly inserted document, including its _id
             res.status(200).send({ id: newDoc._id });
         });
-    }
+    };
 
     exports.getPlanet = function(req, res) {
         console.log('getPlanet, id: ', req.params.id);
@@ -18,15 +18,15 @@
         db.findOne({_id: id }, function(err, planet){
             console.log(planet);
             res.send(planet);
-        })
-    }
+        });
+    };
 
     exports.removePlanet = function(req, res) {
         var id = req.params.id;
         db.remove({_id: id }, function(err, planet){
             res.status(204).send({ status: 'Planet with id, ' + id + ', removed.' });
-        })
-    }
+        });
+    };
 
     exports.getList = function(req, res){
         var system = req.params.system;
@@ -36,7 +36,7 @@
         }
         db.find(search, function(err, docs){
             res.send(docs);
-        })
-    }
+        });
+    };
     
  })();
