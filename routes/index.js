9f0495d4bc4e193a0d30c0c86f7bd047194d8861
cmkanode <cm.kanode@gmail.com>
2017-01-30 21:55:52 -0500

    Added View Engine and Basic Views

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,10 @@
         //app.use('/api/admin', require('./admin'));
         app.use('/api/planets', require('./planets'));
 
-        app.use("/", express.static(path.join(__dirname, '../public')));
+        app.use('/home', function(res, req) {
+            req.render("home", { title: "Home" });
+        });
+        //app.use("/", express.static(path.join(__dirname, '../public')));
         
         // catch 404 and forward to error handler
         app.use(function (req, res, next) {
---
+++ routes/toytracker.js
@@ -0,0 +1,6 @@
+(function(){
+    var express = require('express');
+    var router = express.Router();
+
+    
+})();
